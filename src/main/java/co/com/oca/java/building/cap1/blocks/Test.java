87178d9a6a152d3d1b828b486a78f72fc4e84306
jonathan fernandez <jonathan.fvargas@gmail.com>
2017-02-02 21:15:26 -0500

    First commit

---
+++ src/main/java/co/com/oca/java/building/cap1/blocks/Test.java
@@ -0,0 +1,12 @@
+package co.com.oca.java.building.cap1.blocks;
+
+public class Test {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("hola mundo");
+        
+    }
+}
---
+++ src/main/java/co/com/oca/java/cap2/operators/statements/Statements.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.oca.java.cap2.operators.statements;
+
+/**
+ *
+ * @author Jonathan Fernandez <jonathan.fvargas@gmail.com>
+ */
+public class Statements {
+    
+}
