377b382c9cad83020a4ce2d4d491b071d625ae7d
rezmos <rezmos@gmail.com>
2017-02-11 11:20:53 -0500

    Topic. Classes vs files -> Error example for public access modifier

---
+++ src/main/java/co/com/oca/java/cap1/building/blocks/Test2.java
@@ -0,0 +1,9 @@
+public class Test2{
+/*This class is the only top class in this file that can has the public access modifier
+As you can see the Test3 class has also the public access modifier, that is an error.
+*/
+}
+
+public class Test3{
+//error:class Test3 is public, should be declared in a file named Test3.java
+}
