97505e72ed96ff1fafe0547dba4c8f03cd049828
jonathan fernandez <jonathan.fvargas@gmail.com>
2017-02-12 21:18:48 -0500

    JF add GC package to study

---
+++ src/main/java/co/com/oca/java/cap1/gc/N.java
@@ -0,0 +1,31 @@
+package co.com.oca.java.cap1.gc;
+
+/**
+ * There are no instances of M to be garbage collected till the program ends.
+ *
+ * @author Jonathan Fernandez <jonathan.fvargas@gmail.com>
+ */
+class M {
+}
+
+class N {
+
+    private M m = new M();
+
+    public void makeItNull( M pM ) {
+        System.out.println("2 " + this.m);
+        pM = null;
+        System.out.println("2 " + this.m);
+    }
+
+    public void makeThisNull() {
+        System.out.println("1 " + this.m);
+        makeItNull(m);
+        System.out.println("1 " + this.m);
+    }
+
+    public static void main(String[] args) {
+        N n = new N();
+        n.makeThisNull();
+    }
+}
