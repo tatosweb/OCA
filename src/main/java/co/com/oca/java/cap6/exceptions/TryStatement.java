7ca3abc8feb77c6b6fab4f7bf8b127490a12e902
Claudia Marcela Alvarez Ramos <marcela.alvarez@wnco.com>
2017-07-05 14:31:21 -0500

    Marce - this code is going to fail, please read the comments

--- src/main/java/co/com/oca/java/cap6/exceptions/TryStatement.java
+++ src/main/java/co/com/oca/java/cap6/exceptions/TryStatement.java
@@ -8,23 +8,25 @@ public class TryStatement{
           int num = numArray[-1];
       }finally{
           int num = 0;
-          System.out.println("Even a runtime exception appears in line 6, the finally block is ");
+          System.out.println("Even a runtime exception appears in line 8, the finally block is executed ");
       }
 
     }
     public static void tryCatchFinallyStructureForCheckedException(){
       FileInputStream fis = null;
-      /*This constructor FileInputStream(File filename)
+      /*The constructor called FileInputStream(File filename)
 	     * throws FileNotFoundException which is a checked
 	     * exception*/
        //
       //To make this code compiles properly, just remove the line that starts with REMOVE word and these characters ->*/
-      /*REMOVE
+         /*REMOVE
+
            try{
            //The code in the try block, run normally. If any of the statements throw an exception that can be caught by the exception type listed in the catch block, the try block stops running and execution goes to the catch  statement .
            //If none of the statements in the try block throw an exception that can be caught, the catch clause not run.
-      */
-           fis = new FileInputStream("B:/myfile.txt");
+               */
+
+        fis = new FileInputStream("B:/myfile.txt");
 	         int k;
            /* Method read() of FileInputStream class also throws
 	          * a checked exception: IOException*/
@@ -33,7 +35,7 @@ public class TryStatement{
 		           System.out.print((char)k);
 	         }
            fis.close();
-      /*REMOVE
+        /*REMOVE
 
       }catch(FileNotFoundException  ex){
           System.out.println("FileNotFoundException is a subclass of IOException: "+ex);
@@ -42,7 +44,7 @@ public class TryStatement{
       }finally{
           System.out.println("Finally Block: It'll always happen ");
 	    }
-      */
+*/
     }
     public static void main(String[] args) {
           tryCatchFinallyStructureForCheckedException();
