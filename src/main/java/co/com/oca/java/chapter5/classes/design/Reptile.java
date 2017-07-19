4d544d6c6ccbe7c391a54416e4061ea900c11167
Jhonattan Fernandez <jonathan.fvargas@gmail.com>
2017-07-19 11:32:39 -0500

    [JF] More examples about overload and overriding

--- src/main/java/co/com/oca/java/chapter5/classes/design/Reptile.java
+++ src/main/java/co/com/oca/java/chapter5/classes/design/Reptile.java
@@ -1,16 +1,32 @@
 package co.com.oca.java.chapter5.classes.design;
 
 public class Reptile {
+
 	protected boolean hasLegs() throws InsufficientDataException {
 		throw new InsufficientDataException();
 	}
 
-  protected double getWeight() throws Exception {
-	/*change the method signature and then Snake.java wont compile because
-	 *  getWeight() method on child class is throwing and exception and
-	 *  father class don't
-	 */
-	//protected double getWeight() /*throws Exception*/ {
+	protected double getWeight() throws Exception {
+		/*
+		 * change the method signature and then Snake.java wont compile because getWeight() method on child class is
+		 * throwing and exception and father class don't
+		 */
+		// protected double getWeight() /*throws Exception*/ {
 		return 2;
 	}
+
+	/*
+	 *Second example, uncomment to review
+	 * 
+	 */
+	/*
+	protected double getHeight() throws InsufficientDataException {
+		return 2;
+	}
+
+	protected int getLength() {
+		return 10;
+	}
+	*/
+
 }
--- src/main/java/co/com/oca/java/chapter5/classes/design/Snake.java
+++ src/main/java/co/com/oca/java/chapter5/classes/design/Snake.java
@@ -8,4 +8,32 @@ public class Snake extends Reptile {
 	protected double getWeight() throws InsufficientDataException {
 		return 2;
 	}
+
+	/*
+	 * Uncomment to review
+	 * Second example:
+	 * Since Exception is not
+		a subclass of InsufficientDataException, the third rule of overriding methods is
+		violated and the code will not compile. 
+	 */
+	
+	/*
+	protected double getHeight() throws Exception { // DOES NOT COMPILE
+		return 2;
+	}
+
+	/*
+	 *Uncomment to review
+	 *The getLength() method doesn’t throw an exception in the parent class, but it
+	  does throw an exception, InsufficientDataException, in the child class. In this manner,
+	  the child class defnes a new exception that the parent class did not, which is a violation of
+	  the third rule of overriding methods.
+	 * 
+	 */
+	
+	/*
+	protected int getLength() throws InsufficientDataException { // DOES NOT COMPILE
+		return 10;
+	}*/
+
 }
